3e1d29ff76c496b1072b1f8a8c8a6b32a324f238
Ethan Kennon <206982265+elk1234567@users.noreply.github.com>
2025-06-08 13:17:55 -0700

    add serial output with blink

--- src/main.cpp
+++ src/main.cpp
@@ -29,16 +29,20 @@ int myFunction(int, int);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  // set up serial communication with host device
+  Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED on (HIGH is the voltage level)
+  Serial.println("The LED is On.");   // tell the computer the LED is on
+  delay(1000);                        // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);     // turn the LED off by making the voltage LOW
+  Serial.println("The LED is Off.");  // tell the computer the LED is off
+  delay(1000);                        // wait for a second
 }
 
 
