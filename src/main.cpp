143d3a428e533552141c9b61c2171975af226a78
Ethan Kennon <206982265+elk1234567@users.noreply.github.com>
2025-06-08 15:28:56 -0700

    add serial control of LED

--- src/main.cpp
+++ src/main.cpp
@@ -3,46 +3,43 @@
 // put function declarations here:
 int myFunction(int, int);
 
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
+String message = "blank";
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // set up serial communication with host device
   Serial.begin(9600);
+  while (!Serial){
+    //wait for connection
+  }
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED on (HIGH is the voltage level)
-  Serial.println("The LED is On.");   // tell the computer the LED is on
-  delay(1000);                        // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);     // turn the LED off by making the voltage LOW
-  Serial.println("The LED is Off.");  // tell the computer the LED is off
-  delay(1000);                        // wait for a second
+  // if we get a valid byte, read it:
+  while (Serial.available() > 0) {
+    // get incoming string:
+    message = Serial.readString();
+  }
+
+  if (message.equalsIgnoreCase("on")){
+    digitalWrite(LED_BUILTIN, HIGH);    // turn the LED on (HIGH is the voltage level)
+    Serial.println("The LED is On.");   // tell the computer the LED is on
+  }
+  else if (message.equalsIgnoreCase("off")){
+    digitalWrite(LED_BUILTIN, LOW);     // turn the LED off by making the voltage LOW
+    Serial.println("The LED is Off.");  // tell the computer the LED is off
+  }
+  else if (message == "blank"){
+    // do nothing
+  }
+  else {
+    Serial.println("Unrecognized command");
+  }
+
+  message = "blank";
 }
 
 
