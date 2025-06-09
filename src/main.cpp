c0b3e4a27f0018c677c674e97fd50b1a0b5a80f5
Ethan Kennon <206982265+elk1234567@users.noreply.github.com>
2025-06-09 15:50:06 -0700

    increase baud rate and add toggle

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ char message = '\n';
 // the setup function runs once when you press reset or power the board
 void setup() {
   // set up serial communication with host device
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial){
     //wait for connection
   }
@@ -21,13 +21,22 @@ void loop() {
     // get incoming byte:
     message = Serial.read();
 
-    if (message == 'e'){   //e for enable
+    if (message == 'e' || message == 'E'){   //e for enable
       digitalWrite(LED_BUILTIN, HIGH);    // turn the LED on (HIGH is the voltage level)
       Serial.println("The LED is On.");   // tell the computer the LED is on
     }
-    else if (message == 'd'){  //d for disable
+    else if (message == 'd' || message == 'D'){  //d for disable
       digitalWrite(LED_BUILTIN, LOW);     // turn the LED off by making the voltage LOW
       Serial.println("The LED is Off.");  // tell the computer the LED is off
+    }
+    else if (message == 't' || message == 'T'){  //t for toggle
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));     // turn the LED off by making the voltage LOW
+      if(digitalRead(LED_BUILTIN)){
+        Serial.println("The LED is On.");  // tell the computer the LED is off
+      }
+      else{
+        Serial.println("The LED is Off.");  // tell the computer the LED is off
+      }
     }
     else if (message == '\r' || message == '\n'){
       // do nothing
@@ -39,11 +48,11 @@ void loop() {
 
     message = '\n';
 /*
-    // read the remainder of the message to clear the serial buffer
+    // read the remainder of the message to clear the serial buffer - holds for the timeout so can delay receipt of follow up commands
     while (Serial.available() > 0) {
       // get incoming string:
       String junk = Serial.readString();
-    }
+    } 
 */
   }
 }
